51a8911d09d6a4bb36ee28dbef5a9eb8dfa8aa33
unknown <siriboonruangsiripat@example.com>
2025-09-10 21:58:50 +0700

    change credential to true

--- src/app/libs/axios.ts
+++ src/app/libs/axios.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://20569a98bd05.ngrok-free.app/api"
 
-axios.defaults.withCredentials = false;
+axios.defaults.withCredentials = true;
 
 export default axios;
